68ff4fd263e87a19fc9c60af0f913831627a53fa
kevin-Z01 <zuluaga.kevin@correounivalle.edu.co>
2026-04-04 15:30:33 -0500

    feauture se agrego el mapa para que el admin lo pueda ver la zonas donde tiene sus colabroadores

--- frontend/app/(admin)/Rutas/mapa-Colaboradores.js
+++ frontend/app/(admin)/Rutas/mapa-Colaboradores.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
-import MapView, { Circle } from 'react-native-maps';
+import { WebView } from "react-native-webview";
 import { useState, useEffect } from "react";
 import { ubicacionColaboradores } from "../../../services/colaboradores";
 import { agruparPorCercania } from "../../../services/zonas";
@@ -28,9 +28,39 @@ const MapaColaboradores = () => {
         cargarDatos();
     }, []);
 
-    return (
-        <View>
+    const circulosJS = grupos.map(grupo =>
+        `L.circle([${grupo.centro.lat}, ${grupo.centro.lon}],{radius : 300}).addTo(map);`
+    ).join("\n");
 
+    const marcadoresJS = colaboradores.map(colaborador => 
+        `L.marker([${colaborador.ubicacion_usuario.latidud},${colaborador.ubicacion_usuario.longitud}]).addTo(map);
+        ` ).join("\n");
+    
+
+    const htmlMapa = `
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <style>
+        #map { width: 100%; height: 100vh; }
+    </style>
+</head>
+<body>
+    <div id="map"></div>
+    <script> var map = L.map('map').setView([4.0863,-76.195], 13)
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,}).addTo(map);
+            ${circulosJS}  ${marcadoresJS}
+    </script>
+</body>
+</html>
+`;
+
+    return (
+
+        <View style={styles.mapaContenerdor}>
             {cargando ? (
                 <View style={styles.contenedor}>
                     <ActivityIndicator size={"large"} />
@@ -39,24 +69,9 @@ const MapaColaboradores = () => {
 
             ) : (
 
-                <View>
+                <View style={styles.mapaContenerdor}>
                     <Text>{'Grupos encontrados ' + grupos.length}</Text>
-                    <MapView
-                        style={styles.mapa}
-                        initialRegion={
-                            grupos.length > 0 ? { latitude: grupos[0].centro.lat, longitude: grupos[0].centro.lon, latitudeDelta: 0.05, longitudeDelta: 0.05 }
-                                : { latitude: 4.0863, longitude: -76.195, latitudeDelta: 0.05, longitudeDelta: 0.05 }
-                        }
-                    >
-
-                        {grupos.map(grupo => (
-                            <Circle
-                                fillColor="rgba(76, 175, 80, 0.3)"
-                                strokeColor="#4CAF50" radius={300}
-                                center={{ latitude: grupo.centro.lat, longitude: grupo.centro.lon }}
-                                key={grupo.id} />
-                        ))}
-                    </MapView>
+                    <WebView source={{ html: htmlMapa }} />
                 </View>
 
             )}
@@ -70,6 +85,9 @@ const styles = StyleSheet.create({
     contenedor: {
         justifyContent: 'center',
         alignItems: 'center',
+        flex: 1,
+    },
+    mapaContenerdor: {
         flex: 1,
     },
     mapa: {
