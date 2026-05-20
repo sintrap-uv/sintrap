193d31b6d895f05696718a0c94d11604653c502e
johann199 <johanngonzakez@gmail.com>
2026-05-19 22:47:04 -0500

    bugs asignados del login terminados

--- frontend/services/auth.js
+++ frontend/services/auth.js
@@ -89,3 +89,14 @@ export const verifyResetCode = async (email, token) => {
   })
   return { data, error }
 }
+
+
+export const checkEmail = async (email) => {
+  const { data, error } = await supabase
+  .from("profiles")
+  .select("email")
+  .eq("email", email)
+  .maybeSingle()
+  if (error) return {exists: false, error}
+  return { exists: !!data, error: null}
+}
